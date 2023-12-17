dd5aad2020dea77e5c0ac6381abfe0147fbc5072
Karl Sparks <karl@sparks.to>
2023-12-17 04:10:38 +0000

    Create new structure to support scripts helper functions.

--- scripts.sh
+++ scripts.sh
@@ -1,18 +1,49 @@
 # Add these to your ~/.bash_aliases
+AOC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export AOC
 
-alias aos="python3 solution.py < data/in.txt"
-alias aot="cd $AOC; echo -ne '\\e[0;34m'; python3 solution.py < test.txt; echo -ne '\\e[0m'"
-alias aoc="aot; echo; aos"
+function aoc () {
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Error: Two arguments required (year and day)."
+        echo "Usage: aoc <year> <day>"
+        return 1  # Exit with an error status
+    fi
+
+    local year=$1
+    local day=$2
+
+    printf "\033[0;34m"  # Set text color to blue
+    aot "$year" "$day"
+    printf "\033[0m"  # Reset text color
+    echo
+    aos "$year" "$day"
+}
 
 function aos () {
     local year=$1
     local day=$2
     local inputFile="$AOC/data/input-$year-$day.txt"
+    local solutionFile="$AOC/solutions/$year/python/day-$day.py"
 
-    if [ -f "$inputFile" ]; then
-        python3 solution.py < "$inputFile"
+    if [ -f "$inputFile" ] && [ -f "$solutionFile" ]; then
+        python3 "$solutionFile" < "$inputFile"
     else
-        echo "Input file $inputFile not found."
+        echo "Input file $inputFile or solution file $solutionFile not found."
+        return 1
+    fi
+}
+
+function aot () {
+    local year=$1
+    local day=$2
+    local solutionFile="$AOC/solutions/$year/python/day-$day.py"
+    local testFile="$AOC/data/test-$year-$day.txt"
+
+
+    if [ -f "$solutionFile" ] && [ -f "$testFile" ]; then
+        python3 "$solutionFile" < "$testFile"
+    else
+        echo "Solution file $solutionFile or test file $testFile not found."
         return 1
     fi
 }
@@ -20,6 +51,7 @@ function aos () {
 function aoc-load () {
     export $(cat .env | xargs)
     local inputFile
+    local testFile
     local dataDir="$AOC/data"
 
     # Check if the data directory exists, fail if it does not
@@ -30,23 +62,21 @@ function aoc-load () {
 
     if [ $1 ] && [ $2 ]
     then
-        inputFile="$dataDir/input-$1-$2.txt"
+        local YEAR=$1
+        local DAY=$2
     else
         local YEAR=$(date +%Y)
         local DAY=$(date +%d | sed 's/^0//')  # Remove leading zero if any
-        inputFile="$dataDir/input-$YEAR-$DAY.txt"
     fi
+
+    inputFile="$dataDir/input-$YEAR-$DAY.txt"
+    testFile="$dataDir/test-$YEAR-$DAY.txt"
 
     # Check if the file already exists
     if [ -f "$inputFile" ]; then
         echo "File $inputFile already exists. Skipping download."
     else
-        # Download the file if it doesn't exist
-        if [ $1 ] && [ $2 ]
-        then
-            curl --cookie "session=$AOC_COOKIE" "https://adventofcode.com/$1/day/$2/input" > "$inputFile"
-        else
-            curl --cookie "session=$AOC_COOKIE" "$(echo `date +https://adventofcode.com/%Y/day/%d/input` | sed 's/\/0/\//g')" > "$inputFile"
-        fi
+        curl --cookie "session=$AOC_COOKIE" "https://adventofcode.com/$YEAR/day/$DAY/input" > "$inputFile"
+        touch "$testFile"
     fi
 }
