4408f32e0ba94673297482d91f05dc3558069816
Karl Sparks <karl@sparks.to>
2023-12-29 00:44:04 +0000

    Day 24 part 1 solution

--- solutions/2023/python/day-24.py
+++ solutions/2023/python/day-24.py
@@ -13,7 +13,10 @@ def check_intersection(mx1, mx2, my1, my2):
     return 0 != mx1 * -my2 + mx2 * my1
 
 
-def find_intersection(first, sec, min_test, max_text):
+# x, y, z
+
+
+def find_intersection(first, sec, min_test, max_test):
     pos, vec = first
     bx1, by1, bz1 = pos
     mx1, my1, mz1 = vec
@@ -24,38 +27,52 @@ def find_intersection(first, sec, min_test, max_text):
 
     if check_intersection(mx1, mx2, my1, my2):
         mod = 1 / (mx1 * -my2 + mx2 * my1)
-        s = (-my2 * bx1 + my1 * bx2) * mod
-        t = (mx1 * -by2 + mx2 * by1) * mod
+        s = ((bx1 - bx2) * my1 - (by1 - by2) * mx1) * mod
+        t = ((bx1 - bx2) * my2 - (by1 - by2) * mx2) * mod
 
-        x, y = mx1 * t + bx1, my1 * t + by1
-        print("intersection:", first, sec, x, y)
-        if min_test <= x <= max_test and min_test <= y <= max_text:
-            print("inside test")
+        xs, ys = mx2 * s + bx2, my2 * s + by2
+        xt, yt = mx1 * t + bx1, my1 * t + by1
+        if (
+            min_test <= xs <= max_test
+            and min_test <= ys <= max_test
+            and s >= 0
+            and t >= 0
+        ):
+            return 1
+    return 0
 
 
 # x, y, z
-
-
 test = lambda x, px, py, pz, vx, vy, vz: [
     vx * x + px,
     vy * x + py,
     vz * x + pz,
 ]
 
-min_test, max_test = 7, 27
+if len(vectors) <= 20:
+    min_test, max_test = 7, 27
+else:
+    min_test, max_test = 200000000000000, 400000000000000
 
 check_seen = set()
 formulas = []
 # x, y, z
+ans_1 = 0
 for first in vectors:
     pos, vec = first
     bx1, by1, bz1 = pos
     mx1, my1, mz1 = vec
-    print("P:", pos, vec)
 
     for sec in vectors:
         key = tuple(sorted((first, sec)))
         if first != sec and key not in check_seen:
             check_seen.add(key)
             sec_pos, sec_vec = sec
-            find_intersection(first, sec, min_test, max_test)
+            ans_1 += find_intersection(first, sec, min_test, max_test)
+
+print("Part 1:", ans_1)
+
+ans_2 = 0
+
+
+print("Part 2:", ans_2)
