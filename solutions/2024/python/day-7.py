1a54ff255fa8442ecff460a58c85179788f6e07e
karl-sparks <97003575+karl-sparks@users.noreply.github.com>
2024-12-07 07:51:18 +0000

    Cleaned up day 7 solution

--- solutions/2024/python/day-7.py
+++ solutions/2024/python/day-7.py
@@ -8,15 +8,17 @@ with open(fp_1, encoding="utf-8") as f:
     D = [x.strip() for x in f.readlines()]
 
 
-def calc_options(curr, rem_nums):
+def calc_options(curr, rem_nums, part_2):
     if not rem_nums:
         return {curr}
     
     return_set = set()
     
-    return_set.update(calc_options(curr + rem_nums[0], rem_nums[1:]))
-    return_set.update(calc_options(curr * rem_nums[0], rem_nums[1:]))
-    return_set.update(calc_options(int(str(curr) + str(rem_nums[0])), rem_nums[1:]))
+    return_set.update(calc_options(curr + rem_nums[0], rem_nums[1:], part_2))
+    return_set.update(calc_options(curr * rem_nums[0], rem_nums[1:], part_2))
+
+    if part_2:
+        return_set.update(calc_options(int(str(curr) + str(rem_nums[0])), rem_nums[1:], part_2))
     
     return return_set
 
@@ -28,7 +30,7 @@ def ans_1(D):
         t_value = int(t_value)
         nums = [int(x) for x in nums.split(" ")]
 
-        pos_values = calc_options(nums[0], nums[1:])
+        pos_values = calc_options(nums[0], nums[1:], False)
 
         if t_value in pos_values:
             ans += t_value
@@ -36,3 +38,21 @@ def ans_1(D):
     return ans
 
 print(f"Ans 1: {ans_1(D)}")
+
+
+def ans_2(D):
+    ans = 0
+    
+    for line in D:
+        t_value, nums = line.split(": ")
+        t_value = int(t_value)
+        nums = [int(x) for x in nums.split(" ")]
+
+        pos_values = calc_options(nums[0], nums[1:], True)
+
+        if t_value in pos_values:
+            ans += t_value
+    
+    return ans
+
+print(f"Ans 2: {ans_2(D)}")
