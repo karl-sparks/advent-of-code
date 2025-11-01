e19aaf7e8d2579c6360ae4ee37723b48db0b7899
Karl Sparks <karl@sparks.to>
2025-11-01 21:15:07 +1100

    Working towards part 2 solution

--- solutions/2024/python/day-24.py
+++ solutions/2024/python/day-24.py
@@ -1,80 +1,127 @@
 from collections import deque
 
-wires = {}
 
-class Gate:
-    def __init__(self, type_: str, inputs: list[str], output: str):
-        self.type_ = type_
-        self.inputs = inputs
-        self.output = output
+def get_wire_value(wire_states: dict[str, int], type_of_wire: str) -> str:
+    ans_wires = [wire for wire in wire_states if type_of_wire in wire]
 
-    def apply(self):
-        if self.inputs[0] in wires and self.inputs[1] in wires:
-            match self.type_:
-                case "AND":
-                    wires[self.output] = wires[self.inputs[0]] & wires[self.inputs[1]]
-                case "OR":
-                    wires[self.output] = wires[self.inputs[0]] | wires[self.inputs[1]]
-                case "XOR":
-                    wires[self.output] = wires[self.inputs[0]] ^ wires[self.inputs[1]]
-                case _:
-                    raise ValueError(f"Unknown gate type: {self.type_}")
-            return True
-        return False
+    ans_wires.sort(reverse=True)
 
-def get_wire_value(type_of_wire: str) -> int:
-    ans = 0
-    for wire, value in wires.items():
-        if type_of_wire in wire:
-            sig = int(wire[1:])
-            add_ans = 10**(sig * int(value)) if value else 0
-            ans += add_ans
-    return str(ans)
+    ans = ""
 
-def parse_input(data: str):
+    for wire in ans_wires:
+        ans += str(wire_states[wire])
+
+    return ans
+
+
+def parse_input(
+    data: str,
+) -> tuple[dict[str, str], dict[str, tuple[str, str]], dict[str, int]]:
     initial_wire_state, gates_input = data.split("\n\n")
 
-    gates = []
+    gate_type: dict[str, str] = {}
+    gate_inputs: dict[str, tuple[str, str]] = {}
+    wire_state: dict[str, int] = {}
 
     for line in gates_input.splitlines():
-        input_1, gate_type, input_2, _, output = line.split()
-        gate = Gate(gate_type, [input_1, input_2], output)
-        gates.append(gate)
+        input_1, type, input_2, _, output = line.split()
+        gate_type[output] = type
+        gate_inputs[output] = (input_1, input_2)
 
     for line in initial_wire_state.splitlines():
-        wire, value = line.split(": ")
-        wires[wire] = int(value)
+        wire_id, value = line.split(": ")
+        wire_state[wire_id] = int(value)
 
-    x_v = get_wire_value("x")
-    y_v = get_wire_value("y")
-    print(x_v)
-    print(y_v)
+    return gate_type, gate_inputs, wire_state
 
-    print(str(bin(int(x_v, 2) + int(y_v, 2)))[2:])
 
-    return gates
+def apply_gate(
+    gate: str,
+    gate_type: dict[str, str],
+    gate_inputs: dict[str, tuple[str, str]],
+    wire_state: dict[str, int],
+) -> bool:
+    input_1, input_2 = gate_inputs[gate]
+    if input_1 in wire_state and input_2 in wire_state:
+        match gate_type[gate]:
+            case "AND":
+                wire_state[gate] = wire_state[input_1] & wire_state[input_2]
+            case "OR":
+                wire_state[gate] = wire_state[input_1] | wire_state[input_2]
+            case "XOR":
+                wire_state[gate] = wire_state[input_1] ^ wire_state[input_2]
+            case _:
+                raise ValueError(f"Unknown gate type: {gate_type}")
+        return True
+    return False
 
-def part_one(gates) -> int:
-    print("Running part one")
-    sim_que = deque(gates)
+
+def calc_gates(
+    gate_type: dict[str, str],
+    gate_inputs: dict[str, tuple[str, str]],
+    wire_state: dict[str, int],
+) -> None:
+    sim_que = deque(list(gate_inputs.keys()))
     while sim_que:
         next_gate = sim_que.popleft()
-        if not next_gate.apply():
+        if not apply_gate(next_gate, gate_type, gate_inputs, wire_state):
             sim_que.append(next_gate)
 
-    print("Part one complete")
+    return None
 
-    ans = get_wire_value("z")
 
-    print(ans)
-    
+def get_deps(gate: str, gate_inputs: dict[str, tuple[str, str]]) -> set[str]:
+    deps = set([gate])
+    if gate not in gate_inputs:
+        return deps
+
+    for input in gate_inputs[gate]:
+        deps.update(get_deps(input, gate_inputs))
+
+    return deps
+
+
+def part_one(D: str) -> int:
+    print("Running part one")
+    gate_type, gate_inputs, wire_state = parse_input(D)
+
+    calc_gates(gate_type, gate_inputs, wire_state)
+
+    ans = get_wire_value(wire_state, "z")
+
     return int(ans, 2)
 
+
+def part_two(D: str) -> int:
+    print("Running part two")
+    gate_type, gate_inputs, wire_state = parse_input(D)
+    x_v = get_wire_value(wire_state, "x")
+    y_v = get_wire_value(wire_state, "y")
+
+    true_ans = str(bin(int(x_v, 2) + int(y_v, 2)))[2:]
+
+    calc_gates(gate_type, gate_inputs, wire_state)
+
+    ans = get_wire_value(wire_state, "z")
+
+    ans_diff = str(bin(int(true_ans, 2) ^ int(ans, 2)))[2:]
+    print(true_ans)
+    print(str(ans).zfill(len(true_ans)))
+    print(str(ans_diff).zfill(len(true_ans)))
+    print(list(reversed(ans_diff)))
+
+    deps_1 = get_deps("z01", gate_inputs)
+    deps_2 = get_deps("z02", gate_inputs)
+    print(deps_1)
+    print(deps_2)
+
+    return int(ans, 2)
+
+
 if __name__ == "__main__":
-    print("AOC day 24")
+    print("AOC day 24\n")
     with open(0, encoding="utf-8") as f:
         D = f.read()
 
-    gates = parse_input(D)
-    
-    print(f"Part 1 Ans: {part_one(gates)}")
+    print(f"Part 1 Ans: {part_one(D)}\n")
+    print(f"Part 2 Ans: {part_two(D)}")
